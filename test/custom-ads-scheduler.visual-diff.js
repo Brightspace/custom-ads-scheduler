cfa4aa3acd2256f687d06a347d26cdd8c0fe7abc
Brandon McDole <Brandon.McDole@D2L.com>
2020-09-28 11:58:43 -0400

    Added some basic Visual Diff/Unit testing for the wizard manager page

--- test/custom-ads-scheduler.visual-diff.js
+++ test/custom-ads-scheduler.visual-diff.js
@@ -10,7 +10,7 @@ describe('d2l-manage-schedules', () => {
 	before(async() => {
 		browser = await puppeteer.launch();
 		page = await visualDiff.createPage(browser);
-		await page.setViewport({ width: 800, height: 1000, deviceScaleFactor: 2 });
+		await page.setViewport({ width: 800, height: 2000, deviceScaleFactor: 2 });
 		await page.goto(`${visualDiff.getBaseUrl()}/test/custom-ads-scheduler.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
 	});
@@ -31,4 +31,26 @@ describe('d2l-manage-schedules', () => {
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
+	it('wizard passes visual-diff comparison', async function() {
+		const rect = await visualDiff.getRect(page, '#wizard-manager');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('wizard passes visual-diff comparison, step 2', async function() {
+		await page.$eval('#wizard-manager', (page) => {
+			const wizard = page.children[0].shadowRoot.getElementById('wizard');
+			wizard.next();
+		});
+		const rect = await visualDiff.getRect(page, '#wizard-manager');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('wizard passes visual-diff comparison, step 3', async function() {
+		await page.$eval('#wizard-manager', (page) => {
+			const wizard = page.children[0].shadowRoot.getElementById('wizard');
+			wizard.next();
+		});
+		const rect = await visualDiff.getRect(page, '#wizard-manager');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
 });
