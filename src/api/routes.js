0869573974f2a479c1cfe7453f4f7220ba7e1b0e
Brandon McDole <Brandon.McDole@D2L.com>
2020-10-05 11:49:12 -0400

    Implemented some PR notes

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,5 +2,5 @@ const API_VERSION = "1.0";
 
 export class Routes {
 	static ScheduleLogs(scheduleId, page, count) { return `/d2l/api/customization/${API_VERSION}/ads/scheduler/logs/${scheduleId}/page?page=${page}&count=${count}`; }
-	static NumScheduleLogs(scheduleId, page, count) { return `/d2l/api/customization/${API_VERSION}/ads/scheduler/logs/${scheduleId}/count`; }
+	static NumScheduleLogs(scheduleId) { return `/d2l/api/customization/${API_VERSION}/ads/scheduler/logs/${scheduleId}/count`; }
 }
