5cafe050fa8bfcfa9f1e5b04a12cd19e87a5fbb3
Brandon McDole <Brandon.McDole@D2L.com>
2020-10-14 14:09:35 -0400

    Test for handling 403 exception

--- src/services/addEditScheduleService.js
+++ src/services/addEditScheduleService.js
@@ -3,7 +3,11 @@ import { Scheduler } from '../api/scheduler';
 export class AddEditScheduleService {
 
 	static async getAdvancedDataSets() {
-		const dataSets = await Scheduler.getDataSets();
+		const dataSets = await Scheduler.getDataSets().catch(ex => {
+			window.console.log(ex.detail);
+			return [];
+		});
+
 		return dataSets.filter(ds => {
 			return ds.Category && ds.Category === 'AdvancedDataSets';
 		});
