8fd1d7328616b40eb34e8cf185ec3abf1e5940fe
Brandon McDole <Brandon.McDole@D2L.com>
2020-10-16 17:16:53 -0400

    stringify outgoing schedule

--- src/api/scheduler.js
+++ src/api/scheduler.js
@@ -9,7 +9,7 @@ export class Scheduler {
 	}
 
 	static editSchedule(scheduleId, schedule) {
-		return this._put(Routes.ExistingSchedule(scheduleId), schedule);
+		return this._put(Routes.ExistingSchedule(scheduleId), JSON.stringify(schedule));
 	}
 
 	static getDataSets() {
